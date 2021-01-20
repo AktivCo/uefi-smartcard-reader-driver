384e959eecb7db212df72a96887a71cc017ab493
Eugene Mironenko <mironenko@rutoken.ru>
2021-01-20 19:54:00 +0300

    Fix StrCat not defined

--- SmartCardReaderPkg/SmartCardReader/CcidDriver.c
+++ SmartCardReaderPkg/SmartCardReader/CcidDriver.c
@@ -35,6 +35,7 @@ POSSIBILITY OF SUCH DAMAGE.
 **/
 
 #include <Library/PrintLib.h>
+#include <Library/BaseLib.h>
 #include <Protocol/SmartCardReader.h>
 
 #include "CcidDriver.h"
@@ -374,8 +375,8 @@ InitializeUsbCcidDevice (
       continue;
     }
 
-    StrCpy(UsbCcidDevice->ReaderName, ManufacturerString);
-    StrCat(UsbCcidDevice->ReaderName, L" ");
+    StrCpyS(UsbCcidDevice->ReaderName, sizeof(UsbCcidDevice->ReaderName), ManufacturerString);
+    StrCatS(UsbCcidDevice->ReaderName, sizeof(UsbCcidDevice->ReaderName), L" ");
 
     FreePool(ManufacturerString);
     break;
@@ -393,7 +394,7 @@ InitializeUsbCcidDevice (
       continue;
     }
 
-    StrCat(UsbCcidDevice->ReaderName, ProductString);
+    StrCatS(UsbCcidDevice->ReaderName, sizeof(UsbCcidDevice->ReaderName), ProductString);
 
     FreePool(ProductString);
     break;
@@ -411,7 +412,7 @@ error:
 
   /* Add the Lun */
   UnicodeSPrint(TxtLun, sizeof(TxtLun)/sizeof(TxtLun[0]), L" %d", UsbCcidDevice->Lun);
-  StrCat(UsbCcidDevice->ReaderName, TxtLun);
+  StrCatS(UsbCcidDevice->ReaderName, sizeof(UsbCcidDevice->ReaderName), TxtLun);
 
   /* Length (in bytes) including Null terminator */
   UsbCcidDevice->ReaderNameLength = StrSize(UsbCcidDevice->ReaderName);
@@ -595,7 +596,7 @@ SmartCardReaderDriverBindingStart (
 
     /* Add the slot number */
     UnicodeSPrint(TxtSlot, sizeof(TxtSlot)/sizeof(TxtSlot[0]), L", %d", slot);
-    StrCat(new_UsbCcidDevice->ReaderName, TxtSlot);
+    StrCatS(new_UsbCcidDevice->ReaderName, sizeof(new_UsbCcidDevice->ReaderName), TxtSlot);
 
     /* Set the reader name to the lower level */
     new_reader_index = LunToReaderIndex(new_UsbCcidDevice->Lun);
